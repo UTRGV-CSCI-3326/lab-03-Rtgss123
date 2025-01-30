243d812059702ce20be1d920f366f7a49ab526da
Rtgss123 <134911371+Rtgss123@users.noreply.github.com>
2025-01-30 01:53:10 +0000

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,9 @@
 
+public class BankAccount { 
+    public static void main(String[] args){
+        double bobsmoney = 2175.37;
+        bobsmoney = (((((((bobsmoney - 302.50)+50.03)/2.0)+20.0)-1.0)*2.0)+1.0);
+        System.out.print("Bob's new accout balance is ");
+        System.out.printf("%.2f", bobsmoney);
+    }
+}
